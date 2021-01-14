a959fab97e2c2f33b88781f4a5128edd2c5ae714
EthanTGo <ethango1997@gmail.com>
2021-01-13 20:01:30 -0500

    Modified file to be more original

--- flaskapp/routes.py
+++ flaskapp/routes.py
@@ -1,41 +1,20 @@
 from flaskapp import app
-
 import json, plotly
-from flask import render_template, request, Response, jsonify
+from flask import render_template
 from wrangling_scripts.wrangle_data import return_figures
 
-
-@app.route('/', methods=['POST', 'GET'])
-@app.route('/index', methods=['POST', 'GET'])
+@app.route('/')
+@app.route('/index')
 def index():
 
-	# List of countries for filter
-	country_codes = [['Brunei Darussalam','BRN'],['Cambodia','KHM'],['Indonesia','IDN'],
-	['Laos','LAO'],['Malaysia','MYS'],['Myanmar','MMR'],['Phillipines','PHL'],
-	['Singapore','SGP'],['Thailand','THA'],['Vietnam','VNM']]
+    figures = return_figures()
 
-	# Parse the POST request countries list
-	if (request.method == 'POST') and request.form:
-		figures = return_figures(request.form)
-		countries_selected = []
+    # plot ids for the html id tag
+    ids = ['figure-{}'.format(i) for i, _ in enumerate(figures)]
 
-		for country in request.form.lists():
-			countries_selected.append(country[1][0])
-	
-	# GET request returns all countries for initial page load
-	else:
-		figures = return_figures()
-		countries_selected = []
-		for country in country_codes:
-			countries_selected.append(country[1])
+    # Convert the plotly figures to JSON for javascript in html template
+    figuresJSON = json.dumps(figures, cls=plotly.utils.PlotlyJSONEncoder)
 
-	# plot ids for the html id tag
-	ids = ['figure-{}'.format(i) for i, _ in enumerate(figures)]
-
-	# Convert the plotly figures to JSON for javascript in html template
-	figuresJSON = json.dumps(figures, cls=plotly.utils.PlotlyJSONEncoder)
-
-	return render_template('index.html', ids=ids,
-		figuresJSON=figuresJSON,
-		all_countries=country_codes,
-		countries_selected=countries_selected)
+    return render_template('index.html',
+                           ids=ids,
+                           figuresJSON=figuresJSON)
